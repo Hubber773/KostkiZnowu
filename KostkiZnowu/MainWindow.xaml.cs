2bec6b642063444720da0cbaac34cf7f7768b01e
Hubber773 <githubber53@gmail.com>
2024-12-01 22:43:50 +0100

    Dodaj pliki projektów.

---
+++ KostkiZnowu/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KostkiZnowu
+{
+    public partial class MainWindow : Window
+    {
+        private int punkty;
+        private Kostka kostka;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ResetujGre();
+        }
+
+        private void RzutButton_Click(object sender, RoutedEventArgs e)
+        {
+            punkty += kostka.Rzut();
+            WynikTextBlock.Text = punkty.ToString();
+            GrafikaLabel.Content = kostka.Rzut().ToString();
+        }
+
+        private void ResetButton_Click(object sender, RoutedEventArgs e)
+        {
+            ResetujGre();
+        }
+
+        private void ResetujGre()
+        {
+            punkty = 0;
+            kostka = new Kostka6Scienna();
+            WynikTextBlock.Text = punkty.ToString();
+            GrafikaLabel.Content = "?";
+        }
+    }
+
+    public class Kostka
+    {
+        public virtual int Rzut()
+        {
+            return 0;
+        }
+    }
+
+    public class Kostka6Scienna : Kostka
+    {
+        public override int Rzut()
+        {
+            Random rnd = new Random();
+            return rnd.Next(1, 7);
+        }
+    }
+}
